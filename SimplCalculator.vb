8a24a57ce8bff55c87585e0830aa661ad0aee9a0
unknown <dalekrebel@gmail.com>
2024-01-16 08:29:28 -0700

    Added Variables and Readline

--- SimplCalculator.vb
+++ SimplCalculator.vb
@@ -2,11 +2,21 @@
 'RCET 1265
 'Spring 2024
 'Simple Calculator
-'
+'https://github.com/Masaharu41/Simple-Calculator.git 
 
 Module SimplCalculator
 
     Sub Main()
+        'Assign Variables
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Console.WriteLine("First Numeric Input:")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Second Numeric Input:")
+        num2 = Console.ReadLine()
+        Console.WriteLine($"You have {num1} & {num2}")
+
+        Console.Read()
 
     End Sub
 
